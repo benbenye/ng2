c3b7655d6438740deb4eeac3d9e71af7557897d7
benbenye <1203846281@qq.com>
2016-09-17 22:43:51 +0800

    根据访问平台区分入口文件

--- server/index.js
+++ server/index.js
@@ -6,9 +6,26 @@ let config = require('./config');
 let router = require('./router');
 let proxy = require('koa-proxy');
 let pixie = require('koa-pixie-proxy');
-const Router = require('koa-router');
+let send = require('koa-send');
+let path = require('path');
 
 let app = koa();
+
+app.use(function *(next){
+  let m = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"],
+      useragent = this.req.headers['user-agent'];
+  if(this.path == '/'){
+    if(m.some(function(i){return useragent.match(i);})){
+      console.log('m')
+      yield send(this, path.join(this.path,'dist/client/index_m.html'));
+    }else{
+      console.log('pc')
+      yield send(this, path.join(this.path,'dist/client/index.html'));
+    }
+  } else {
+    yield next
+  }
+})
 
 app.use(staticServe(config.staticPath));
 
