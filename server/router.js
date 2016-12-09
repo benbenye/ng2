e9b96f93be88fd7efcd90a6b507db917a43df156
benbenye <1203846281@qq.com>
2016-12-09 18:09:19 +0800

    单次可以下载文件了

--- server/router.js
+++ server/router.js
@@ -49,64 +49,47 @@ router.get('/crawlers', function*() {
     info[i].href = $(this).attr('href')
     info[i].title = $(this).attr('title')
   });
+  result.arr = info;
 
   // 获取单章漫画的所有图片
   let pics = request.get('http://www.pufei.net'+info[0].href).charset('gbk');
   let picsRes = yield pics;
-  var p = picsRes.text.match(/function base64decode.*/)[0];
-  console.log(p)
-  eval(p)
-  // var photosr = new Array();
-  // eval(picsRes.text.match(/packed=.*slice\(\d\)\)\)\;/)[0])
-  console.log(typeof p)
-  fs.writeFile('html.html',picsRes.text,function (err) {
-    if(err) throw err;
-    console.log('ok')
-  })
 
-  // let reg = /new Array.*,''\)/;
-  // let mhCap = _result.text.match(reg)[0];
-  // let _arr = eval(mhCap);
-  // result.arr = _arr.map(e=>{
-  //   let $ = cheerio.load(e);
-  //
-  //   return {
-  //     href:$('a').attr('href'),
-  //     title:$('a').attr('title')
-  //   }
-  // });
-  // for(let i = 0, l = result.arr.length-1; i < l; ++i){
-  //   let qq = request.get('http://m.neihanlu.com'+result.arr[i].href);
-  //   let qqq = yield qq;
-  //   let _q = qqq.text.match(/var picTree.*\.jpg\'\]\;/)[0];
-  //   result.arr[i].pic = eval(_q.replace('var picTree =',''));
-  //   let pic_base = 'http://manhua.mimanhua.com/';
-    // for(let j = 0, jl = result.arr[i].pic.length; j < jl; ++j){
-    //   let _pic = request.get(pic_base+result.arr[i].pic[j]);
-    //   let _picRes = yield _pic;
-    //   var path = 'imgfile/'+result.arr[i].title+'/'+j+'.jpg';
-      // fs.exists(path, function (err) {
-      //   if(err){
-      //     fs.mkdir('imgfile/'+result.arr[i].title,function (err) {
-      //       if(err) throw err;
-      //       fs.writeFile(path, _picRes.body, (err) => {
-      //         if (err) throw err;
-      //         console.log('It\'s saved!');
-      //       });
-      //     })
-      //   }else{
-      //     fs.writeFile(path, _picRes.body, (err) => {
-      //       if (err) throw err;
-      //       console.log('It\'s saved!');
-      //     });
-      //   }
-      //
-      // })
-    //   console.log(fs.existsSync(path));
-    //   console.log(_picRes.body)
-    //
-    // }
-  // }
+
+  for(let i = 0, l = result.arr.length-1; i < l; ++i) {
+    let picsReq = request.get('http://www.pufei.net' + info[0].href).charset('gbk');
+    let picsRes = yield picsReq;
+    var p = picsRes.text.match(/function base64decode.*/)[0];
+    var s = {};
+    p = p.replace('function base64decode', 's.base64decode = function')
+    eval(p);
+    let photosr = new Array();
+    let base64decode = s.base64decode;
+    let _paced = picsRes.text.match(/packed=.*slice\(\d\)\)\)\;/)[0];
+    _paced = _paced.replace('packed=', 'let packed=')
+    eval(_paced);
+
+    result.arr[i].pic = photosr;
+
+    for (let j = 1, jl = result.arr[i].pic.length; j <= jl; ++j) {
+      let _pic = request.get('http://ziniao.zgkouqiang.cn/' + result.arr[i].pic[j]);
+      let _picRes = yield _pic;
+      let path = 'imgfile/' + result.arr[i].title;
+      let imgPath = path+ '/' + j + '.jpg';
+      fs.writeFile(imgPath, _picRes.body, (err) => {
+        if (err){
+          fs.mkdir(path, function (err) {
+            if (err) throw err;
+            fs.writeFile(imgPath, _picRes.body, (err) => {
+              if (err) throw err;
+              console.log('It\'s saved!');
+            });
+          })
+        }
+        console.log('It\'s saved!');
+      });
+    }
+  }
 
   this.body = result;
 });
