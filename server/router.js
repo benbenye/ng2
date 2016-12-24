e13bcfb2f2dbe1877f713067b0b7c3929dbfa1d1
wmzy <1256573276@qq.com>
2016-12-24 16:49:45 +0800

    download-img3-1.0

--- server/router.js
+++ server/router.js
@@ -41,8 +41,48 @@ function findPost(id) {
 }
 
 router.get('/crawlers', function*() {
+  function makeReuest2(option){
+    return  request.get(option.url)
+      .set('Accept','image/webp,*/*;q=0.8')
+      .set('Accept-Encoding','gzip, deflate')
+      .set('Accept-Language','zh-CN,zh;q=0.8,en;q=0.6')
+      .set('Cache-Control','no-cache')
+      .set('Connection','keep-alive')
+      .set('Cookie',option.cookie)
+      .set('Host',option.host)
+      .set('Pragma','no-cache')
+      .set('Referer',option.referer)
+      // .set('Upgrade-Insecure-Requests','1')
+      .set('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36')
+      .catch(option.cb)
+      ;
+
+  }
+  function makeReuest(option){
+    return  request.get(option.url).charset('gbk')
+      .set('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+      // .set('Accept-Encoding','gzip, deflate, sdch')
+      .set('Accept-Language','zh-CN,zh;q=0.8,en;q=0.6')
+      .set('Cache-Control','no-cache')
+      .set('Connection','keep-alive')
+      .set('Cookie',option.cookie)
+      .set('Host',option.host)
+      .set('Pragma','no-cache')
+      .set('Referer',option.referer)
+      // .set('Upgrade-Insecure-Requests','1')
+      .set('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36')
+      .catch(option.cb)
+      ;
+
+  }
   let result = {success: true,arr:[]};
-  let ss = request.get('http://www.pufei.net/manhua/20/').charset('gbk');
+  let ss = makeReuest({
+    url: 'http://www.pufei.net/manhua/20/',
+    cookie:'__cfduid=d8ee8af0943f32d523ae9165ac4ef57351481265253; imgserver=1; playeds=20%7C6526%7C%E9%95%87%E9%AD%82%E8%A1%97%7C171%20%E6%9B%B9%E7%8E%84%E4%BA%AE04%7C1482217980%7C20; Hm_lvt_230837fdf572deef0b702c931922583f=1481265282; Hm_lpvt_230837fdf572deef0b702c931922583f=1482217981',
+    host:'www.pufei.net',
+    referer:'http://www.pufei.net/manhua/',
+    cb:function(err){console.log('获取章节：'+err.message)}
+  });
   let _result = yield ss;
   let $ = cheerio.load(_result.text)
   let _dom = $('#play_0')
@@ -52,21 +92,19 @@ router.get('/crawlers', function*() {
     info[i].href = $(this).attr('href')
     info[i].title = $(this).attr('title')
   });
-
+  
+  result.arr = info;
+  console.log('------------', info.length)
   for(let i = 0, l = result.arr.length-1; i < l; ++i) {
-    let picsReq = request.get('http://www.pufei.net' + info[i].href).charset('gbk')
-      .set('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-      .set('Accept-Encoding','gzip, deflate, sdch')
-      .set('Accept-Language','zh-CN,zh;q=0.8,en;q=0.6')
-      .set('Cache-Control','no-cache')
-      .set('Connection','keep-alive')
-      .set('Cookie','__cfduid=d8ee8af0943f32d523ae9165ac4ef57351481265253; imgserver=1; playeds=20%7C6526%7C%E9%95%87%E9%AD%82%E8%A1%97%7C171%20%E6%9B%B9%E7%8E%84%E4%BA%AE04%7C1482217980%7C20; Hm_lvt_230837fdf572deef0b702c931922583f=1481265282; Hm_lpvt_230837fdf572deef0b702c931922583f=1482217981')
-      .set('Host','www.pufei.net')
-      .set('Pragma','no-cache')
-      .set('Referer','http','//www.pufei.net/manhua/20/')
-      .set('Upgrade-Insecure-Requests','1')
-      .set('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36');
+    let picsReq = makeReuest({
+      url: 'http://www.pufei.net' + info[i].href,
+      cookie:'__cfduid=d8ee8af0943f32d523ae9165ac4ef57351481265253; imgserver=1; playeds=20%7C6526%7C%E9%95%87%E9%AD%82%E8%A1%97%7C171%20%E6%9B%B9%E7%8E%84%E4%BA%AE04%7C1482217980%7C20; Hm_lvt_230837fdf572deef0b702c931922583f=1481265282; Hm_lpvt_230837fdf572deef0b702c931922583f=1482217981',
+      host:'www.pufei.net',
+      referer:'http://www.pufei.net/manhua/20',
+      cb:function(err){console.log("第"+info[i].title+""+err.message)}
+    });
     let picsRes = yield picsReq;
+    if(!picsRes) continue;
     var p = picsRes.text.match(/function base64decode.*/)[0];
     var s = {};
     p = p.replace('function base64decode', 's.base64decode = function')
@@ -79,42 +117,48 @@ router.get('/crawlers', function*() {
 
     result.arr[i].pic = photosr;
 
+    console.log('....', i)
     for (let j = 1, jl = result.arr[i].pic.length; j <= jl; ++j) {
-      let _pic = request.get('http://ziniao.zgkouqiang.cn/' + result.arr[i].pic[j])
-        .set('Accept', 'mage/webp,image/*,*/*;q=0.8')
-        .set('Accept-Encoding','gzip, deflate, sdch')
-        .set('Accept-Language','zh-CN,zh;q=0.8,en;q=0.6')
-        .set('Cache-Control','no-cache')
-        .set('Connection','keep-alive')
-        .set('Cookie','__cfduid=daefd4d754d9c58038f7264cdd2b704b81481265253')
-        .set('Host','ziniao.zgkouqiang.cn')
-        .set('Pragma','no-cache')
-        .set('Referer','http,//www.pufei.net'+info[i].href)
-        .set('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36');
+      console.log('..', j)
+      let _pic = makeReuest2({
+            url: 'http://ziniao.zgkouqiang.cn/' + result.arr[i].pic[j],
+            cookie:'_cfduid=dc43da4fb68265e15bb28840d3550a9c31480774326',
+            host:'ziniao.zgkouqiang.cn',
+            referer:'http://www.pufei.net'+info[i].href,
+            cb:function(err){console.log('图片：'+err.message)}
+        })
+        ;
       let _picRes = yield _pic;
+      if(!_picRes) continue;
       let path = 'imgfile/' + result.arr[i].title;
       let imgPath = path+ '/' + j + '.jpg';
 
       const writeFileAsync = promisify(fs.writeFile);
       const mkdirAsync = promisify(fs.mkdir);
 
+      console.log('write start:', 'http://ziniao.zgkouqiang.cn/' + result.arr[i].pic[j])
+      console.log('res body: ', Buffer.isBuffer(_picRes), _picRes.buffered)
       writeFileAsync(imgPath, _picRes.body)
         .then(function () {
-          console.log(imgPath)
+          console.log('write OK:', imgPath)
         })
         .catch(function () {
+          console.log('write err:', imgPath)
           mkdirAsync(path)
             .then(function () {
-              writeFileAsync(imgPath, _picRes.body)
-                .then(function () {
-                  console.log(imgPath)
-                })
-                .catch(function (err) {
-                  throw  err;
-                })
+              return writeFileAsync(imgPath, _picRes.body)
             })
-            .catch();
+            .then(function () {
+               console.log('write file retry finished:', imgPath)
+            })
+            .catch(function (err) {
+              console.log('retry err', err)
+              throw  err;
+            });
         });
+        yield (new Promise((res, rej)=>{
+          setTimeout(res, 2000+Math.random())
+        }));
     }
   }
   result.arr = info;
